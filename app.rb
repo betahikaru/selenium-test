d5e2b25c8f61d343d0d4bd193268231294825ac8
betahikaru <beta.hikaru2000+github@gmail.com>
2014-08-28 00:35:10 +0900

    betahikaru.com access (notfood)

--- app.rb
+++ app.rb
@@ -12,4 +12,22 @@ def search_google_demo
   driver.close
 end
 
+def display_betahikarucom_demo
+  driver = Selenium::WebDriver.for :firefox
+  driver.get "http://betahikaru.com"
+  begin
+    driver.find_element(:partial_link_text, "動物は").click
+    sleep 3
+
+    driver.find_element(:id, "id_sentence1").send_key "title"
+    driver.find_element(:id, "id_sentence2").send_key "subtitle"
+
+  rescue => e
+    puts e.to_s
+  ensure
+    driver.close
+  end
+end
+
 search_google_demo
+display_betahikarucom_demo
